ab906c81744e618e5c0242f3f9c4929391a1de34
Anusha Kamat <anushasanyamkamat@gmail.com>
2020-06-06 11:08:05 +0200

    :x

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <p>
+            <b>Name</b>: <i>Anusha Kamat </i><br>
+            <b>Age</b>: <i>28 </i><br>
+            <b>Address</b>: <i>Berlin, Germany</i>
+        </p>
+            
+    </div>
+</template>
+
+<script>    
+export default {
+    
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+import UserInfo from '@/components/UserInfo.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HelloWorld
+  },
+  {
+    path: '/user',
+    name: 'userInfo',
+    component: UserInfo
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
